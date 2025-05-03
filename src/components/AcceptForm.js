f15bc2ed01d1324d3e4a3b37643c7cee8c73dca9
shovonroy173 <shovonroy2003@gmail.com>
2025-05-03 20:46:46 +0600

    mod: dyami data added

--- src/components/AcceptForm.js
+++ src/components/AcceptForm.js
@@ -1,9 +1,20 @@
 import Image from "next/image";
 import React from "react";
 
-function AcceptForm({sitename}) {
-    console.log('sitename', sitename);
-    
+function AcceptForm({ sitename }) {
+  console.log("sitename", sitename);
+  const time = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const period = hours >= 12 ? "pm" : "am";
+
+    // Convert to 12-hour format
+    const formattedHour = hours % 12 || 12; // 0 becomes 12
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+
+    return `${formattedHour}.${formattedMinutes} ${period}`;
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="shadow-md rounded-md flex flex-col justify-center items-center  max-w-[500px] w-full gap-4 py-14 px-5">
@@ -14,15 +25,15 @@ function AcceptForm({sitename}) {
           height={100}
           className="rounded-full w-20 h-20 object-cover"
         />
-        {/* <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
           <p className="font-semibold">{sitename.name}</p>
-          <p>Payment from {sitename.tag}</p>
+          <p>Payment from {sitename.cashTag}</p>
         </div>
         <p className="font-medium text-4xl">$ {sitename.amount}</p>
         <div className="flex flex-col items-center">
           <p className="text-gray-500">For Apt</p>
-          <p className="text-gray-500">{info.date}</p>
-        </div> */}
+          <p className="text-gray-500">Today {time()}</p>
+        </div>
         <div className="flex flex-col gap-4 w-full items-center">
           <button className="bg-green-700 text-white rounded-full w-4/5 py-2 hover:bg-green-600 text-2xl">
             Accept
