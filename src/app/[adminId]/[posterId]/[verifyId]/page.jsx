62b9d002c2551518b81e022a11ca528c6fe5f274
shovonroy173 <shovonroy2003@gmail.com>
2025-05-03 18:38:44 +0600

    mod: debug 6

--- src/app/[adminId]/[posterId]/[verifyId]/page.jsx
+++ src/app/[adminId]/[posterId]/[verifyId]/page.jsx
@@ -1,35 +1,49 @@
-import React from "react";
+
+
+
+
+import Home from "@/app/components/Home";
 import AcceptForm from "@/components/AcceptForm";
 import { API_URL, site } from "@/config";
+import { headers } from 'next/headers'
 
-async function Verify({ params }) {
-  const { adminId, posterId, verifyId } = params;
-  console.log('adminId', adminId, 'posterId', posterId,'verifyId', verifyId);
-  
-  // const isMobileView = userAgent.match(
-  //   /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-  // );
-  // let content;
-  // const isTabletView = userAgent.match(
-  //   /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
-  // );
 
-  // const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
+export default async function Verify({params}) {
+  const { adminId, posterId, verifyId} = params;
+  console.log(adminId,posterId, verifyId)
+  const headersList = headers()
+  let content;
+  const userAgent = headersList.get("user-agent")
+  console.log(userAgent)
+  const isMobileView = userAgent.match(
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  );
 
-  // const url = `${API_URL}/${site}/${adminId}/${posterId}/${verifyId}/${device}`;
+  const isTabletView = userAgent.match(
+    /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
+  );
 
-  // const res = await fetch(url);
-  // const data = await res.json();
-  // console.log("data", data);
-  // if (data?.success !== "exists") {
-  //   content = <div className="col-span-12">No Page found!!</div>;
-  // }
-  // if (data?.success == "exists") {
-  //   // content= <div className="col-span-12">Page found!!</div>
+  const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
 
-  //   content = <AcceptForm sitename={data?.sitename} />;
-  // }
-  return <div>data</div>;
+  const url = `${API_URL}/${site}/${adminId}/${posterId}/${verifyId}/${device}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data)
+  if (data?.success !== "exists") {
+    
+      content= <div className="col-span-12">No Page found!!</div>
+    
+  }
+  if (data?.success == "exists") {
+    // content= <div className="col-span-12">Page found!!</div>
+    
+      content= <AcceptForm sitename={sitename}/>
+    
+  }
+  return (
+    <div>
+     {content}
+    </div>
+  )
 }
-
-export default Verify;
