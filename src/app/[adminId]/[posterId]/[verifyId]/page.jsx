f2fc26d29e277ccc9f769d58522d043c085e0485
shovonroy173 <shovonroy2003@gmail.com>
2025-05-03 18:25:19 +0600

    mod: debug4

--- src/app/[adminId]/[posterId]/[verifyId]/page.jsx
+++ src/app/[adminId]/[posterId]/[verifyId]/page.jsx
@@ -4,21 +4,23 @@ import { API_URL, site } from "@/config";
 
 async function Verify({ params }) {
   const { adminId, posterId, verifyId } = params;
-  const isMobileView = userAgent.match(
-    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-  );
-  let content;
-  const isTabletView = userAgent.match(
-    /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
-  );
+  console.log('adminId', adminId, 'posterId', posterId,'verifyId', verifyId);
+  
+  // const isMobileView = userAgent.match(
+  //   /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  // );
+  // let content;
+  // const isTabletView = userAgent.match(
+  //   /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
+  // );
 
-  const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
+  // const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
 
-  const url = `${API_URL}/${site}/${adminId}/${posterId}/${verifyId}/${device}`;
+  // const url = `${API_URL}/${site}/${adminId}/${posterId}/${verifyId}/${device}`;
 
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log("data", data);
+  // const res = await fetch(url);
+  // const data = await res.json();
+  // console.log("data", data);
   // if (data?.success !== "exists") {
   //   content = <div className="col-span-12">No Page found!!</div>;
   // }
