67feb77cba73754541120e1ef34a0f7353efa26d
shovonroy173 <shovonroy2003@gmail.com>
2025-05-03 19:14:59 +0600

    mod:commit

--- src/app/[adminId]/[posterId]/[verifyId]/page.jsx
+++ src/app/[adminId]/[posterId]/[verifyId]/page.jsx
@@ -1,43 +1,40 @@
-import Image from "next/image";
-import { useRouter } from "next/router";
 import React from "react";
+import AcceptForm from "@/components/AcceptForm";
+import { API_URL, site } from "@/config";
 
-function AcceptForm() {
-  const {query} = useRouter();
-  const { adminId, posterId , verifyId} = query;
-  console.log(adminId,posterId, verifyId)
-  console.log('data');
+async function Verify({ params }) {
+  const { adminId, posterId, verifyId } = await params;
+  console.log(adminId, posterId, verifyId);
 
+  // const isMobileView = userAgent.match(
+  //   /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  // );
+  // let content;
+  // const isTabletView = userAgent.match(
+  //   /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
+  // );
+
+  // const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
+
+  // const url = `${API_URL}/${site}/${adminId}/${posterId}/${verifyId}/${device}`;
+
+  // const res = await fetch(url);
+  // const data = await res.json();
+  // console.log(data);
+  // if (data?.success !== "exists") {
+  //   content = <div className="col-span-12">No Page found!!</div>;
+  // }
+  // if (data?.success == "exists") {
+  //   // content= <div className="col-span-12">Page found!!</div>
+
+  //   content = <AcceptForm sitename={data?.sitename} />;
+  // }
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="shadow-md rounded-md flex flex-col justify-center items-center  max-w-[500px] w-full gap-4 py-14 px-5">
-        <Image
-          src="/side hustle.webp"
-          alt="img"
-          width={100}
-          height={100}
-          className="rounded-full w-20 h-20 object-cover"
-        />
-        {/* <div className="flex flex-col items-center">
-          <p className="font-semibold">{sitename.name}</p>
-          <p>Payment from {sitename.tag}</p>
-        </div>
-        <p className="font-medium text-4xl">$ {sitename.amount}</p>
-        <div className="flex flex-col items-center">
-          <p className="text-gray-500">For Apt</p>
-          <p className="text-gray-500">{info.date}</p>
-        </div> */}
-        <div className="flex flex-col gap-4 w-full items-center">
-          <button className="bg-green-700 text-white rounded-full w-4/5 py-2 hover:bg-green-600 text-2xl">
-            Accept
-          </button>
-          <button className="bg-yellow-700 text-white rounded-full w-4/5 py-2 hover:bg-yellow-600 text-2xl">
-            Decline
-          </button>
-        </div>
-      </div>
+    <div>
+      {/* {content} */}
+      data
     </div>
   );
 }
 
-export default AcceptForm;
+export default Verify;
